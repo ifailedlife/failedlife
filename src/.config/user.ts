8d2676d1a93aa91f448d337d93202e68a952eab6
ifailedlife <i@failed.life>
2024-11-09 09:19:37 +0800

    Update user.ts
    changed basic settings.

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -4,4 +4,92 @@ export const userConfig: Partial<UserConfig> = {
   // Override the default config here
   // site: { title: "講評世界" },
   // seo: { twitter: "@moeyua13" },
+
+  
+  site: {
+    title: '받는이 없음',
+    subtitle: '적도친구',
+    author: '무명씨',
+    description: '받는이가 없는 편지 모음입니다.',
+    website: 'https://failed.life',
+    pageSize: 5,
+    socialLinks: [
+      // {
+      //   name: 'github',
+      //   href: 'https://github.com/moeyua/astro-theme-typography',
+      // },
+      {
+        name: 'rss',
+        href: '/atom.xml',
+      },
+      // {
+      //   name: 'twitter',
+      //   href: 'https://github.com/moeyua/astro-theme-typography',
+      // },
+      // {
+      //   name: 'mastodon',
+      //   href: 'https://github.com/moeyua/astro-theme-typography',
+      // },
+    ],
+    navLinks: [
+      {
+        name: 'Posts',
+        href: '/',
+      },
+      {
+        name: 'Archive',
+        href: '/archive',
+      },
+      {
+        name: 'Categories',
+        href: '/categories',
+      },
+      {
+        name: 'About',
+        href: '/about',
+      },
+    ],
+    categoryMap: [{ name: '胡适', path: 'hu-shi' }],
+    footer: [
+      '© %year <a target="_blank" href="%website">%author</a>',
+      'Theme <a target="_blank" href="https://github.com/Moeyua/astro-theme-typography">Typography</a> by <a target="_blank" href="https://moeyua.com">Moeyua</a>',
+      'Proudly published with <a target="_blank" href="https://astro.build/">Astro</a>',
+    ],
+  },
+  appearance: {
+    theme: 'dark',
+    locale: 'zh-cn',
+    colorsLight: {
+      primary: '#2e405b',
+      background: '#ffffff',
+    },
+    colorsDark: {
+      primary: '#FFFFFF',
+      background: '#232222',
+    },
+    fonts: {
+      header:
+        '"HiraMinProN-W6","Source Han Serif CN","Source Han Serif SC","Source Han Serif TC",serif',
+      ui: '"Source Sans Pro","Roboto","Helvetica","Helvetica Neue","Source Han Sans SC","Source Han Sans TC","PingFang SC","PingFang HK","PingFang TC",sans-serif',
+    },
+  },
+  seo: {
+    twitter: '@moeyua13',
+    meta: [],
+    link: [],
+  },
+  rss: {
+    fullText: true,
+  },
+  comment: {
+    // disqus: { shortname: "typography-astro" },
+  },
+  analytics: {
+    googleAnalyticsId: '',
+    umamiAnalyticsId: '',
+  },
+  latex: {
+    katex: false,
+  },
+
 }
